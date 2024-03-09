657df3f81499240e2afb7bd7257ec3ce4da2b754
leonard77-pinto <leonard77.pinto@gmail.com>
2024-03-09 06:33:33 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import { app } from './app';
 import { env } from './env';
+// import { PrismaClient } from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
 
 app.listen({
 	host: '0.0.0.0',
 	port: env.PORT
-}).then(()=>{
+}).then(async ()=>{
+	// const user = await prisma.user.create({
+	// 	data:{
+	// 		name: 'joana',
+	// 		email: 'joana@email.com'
+	// 	}
+	// });
+	
+	// console.log(user);
 	console.log('🆙 Server UP...');
 });
